8353bf5be47a8c6cd8b2a2fa3418c9eb745b9d1c
Hang Shi <hangshiisi@gmail.com>
2015-09-30 13:47:54 -0700

    filter implementation

---
+++ python/nfq.py
@@ -0,0 +1,33 @@
+import nfqueue
+from scapy.all import *
+import os
+domain = 'facebook.com'
+os.system('iptables -A OUTPUT -p udp --dport 53 -j NFQUEUE')
+def callback(payload):
+    data = payload.get_data()
+    pkt = IP(data)
+    if not pkt.haslayer(DNSQR):
+        payload.set_verdict(nfqueue.NF_ACCEPT)
+    else:
+        if domain in pkt[DNS].qd.qname:
+            spoofed_pkt = IP(dst=pkt[IP].src, src=pkt[IP].dst)/\
+                          UDP(dport=pkt[UDP].sport, sport=pkt[UDP].dport)/\
+                          DNS(id=pkt[DNS].id, qr=1, aa=1, qd=pkt[DNS].qd,\
+                          an=DNSRR(rrname=pkt[DNS].qd.qname, ttl=10, rdata=localIP))
+            payload.set_verdict_modified(nfqueue.NF_ACCEPT, str(spoofed_pkt), len(spoofed_pkt))
+            print '[+] Sent spoofed packet for %s' % domain
+def main():
+    q = nfqueue.queue()
+    q.open()
+    q.bind(socket.AF_INET)
+    q.set_callback(callback)
+    q.create_queue(0)
+    try:
+        q.try_run() # Main loop
+    except KeyboardInterrupt:
+        q.unbind(socket.AF_INET)
+        q.close()
+        os.system('iptables -F')
+        os.system('iptables -X')
+        sys.exit('losing...')
+main()

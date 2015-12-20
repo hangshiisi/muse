20189acccdad518fbe7a8d2767217f8922a48bbb
Hang Shi <hangshiisi@gmail.com>
2015-12-20 05:57:00 -0800

    openstack config file

---
+++ OS/admin-openrc.sh
@@ -0,0 +1,5 @@
+export OS_TENANT_NAME=admin
+export OS_USERNAME=admin
+export OS_PASSWORD=cisco123
+export OS_AUTH_URL=http://controller:35357/v2.0
+
---
+++ OS/demo-openrc.sh
@@ -0,0 +1,5 @@
+export OS_TENANT_NAME=demo
+export OS_USERNAME=demo
+export OS_PASSWORD=demo
+export OS_AUTH_URL=http://controller:5000/v2.0
+
